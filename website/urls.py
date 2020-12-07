778bab7e679f88b2ad51391d85b9a74f36b9fb24
raymondyan <raymond.dream@gmail.com>
2020-12-07 20:00:57 +0800

    hhh

--- website/urls.py
+++ website/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('education', views.education, name='education'),
     path('about', views.about_us, name='about_us'),
     path('health_care', views.health, name='health'),
-    path('show_1', views.show_1(), name='show_1')
+    path('show_1', views.show_1, name='show_1')
 ]
